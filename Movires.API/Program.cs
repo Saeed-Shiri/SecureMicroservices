f35a986b3e2225c5ef172437b05ccd13bc9f8db2
s.shiri <s.shiri@IFBdev.local>
2025-03-25 13:54:07 +0330

    seed movies api database

--- Movires.API/Program.cs
+++ Movires.API/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Movies.API.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MoviesAPIContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MoviesAPIContext") ?? throw new InvalidOperationException("Connection string 'MoviesAPIContext' not found.")));
+    options.UseInMemoryDatabase("Movies"));
 
 // Add services to the container.
 
@@ -27,4 +27,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await SeedDatabase(app.Services);
+
 app.Run();
+
+async Task SeedDatabase(IServiceProvider service)
+{
+    await using var scope = service.CreateAsyncScope();
+    var context = scope.ServiceProvider.GetRequiredService<MoviesAPIContext>();
+
+    await MoviesContextSeed.SeedAsync(context);
+}
